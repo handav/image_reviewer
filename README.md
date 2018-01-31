451ad228c9780e070f444832fb4da5f58da9c560
handav <hannahdavis0220@gmail.com>
2018-01-31 17:08:22 -0500

    fixing text overlay

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-Takes a downloaded CSV from Crowdflower in this format:
-_unit_id,_created_at,_id,_started_at,_tainted,_channel,_trust,_worker_id,_country,_region,_city,_ip,emotion_types,category,emotion_types_gold,image_url
+Takes a downloaded 'full results' CSV from a Crowdflower job, and looks at the relevant image from your computer.
--- review.py
+++ review.py
@@ -5,16 +5,19 @@ import sys
 from pathlib import Path
 
 path_to_saved_images = '/Volumes/Seagate Backup Plus Drive/Hannah_Data/Datasets/AI_Grant/'
-results_csv = 'f1227153.csv'
+results_csv = 'cf_report_1230552_full.csv'
 
 keyword_index = 14
 keyword_options = ['city', 'field', 'forest', 'mountain', 'ocean', 'lake', 'road']
 
-font                   = cv2.FONT_HERSHEY_SIMPLEX
-corner                 = (10,100)
-fontScale              = 1
-fontColor              = (255,255,255)
-lineType               = 2
+font = cv2.FONT_HERSHEY_SIMPLEX
+corner = (10,200)
+fontScale = 1
+fontColor = (255,255,255)
+lineType = 2
+
+current_image_name = ''
+current_image_counter = 1
 
 def open_csv_results(csv_file):
     with open(csv_file, 'rb') as f:
@@ -29,16 +32,37 @@ def open_csv_results(csv_file):
     return [results , emotion_types_index, image_url_index]
 
 def process_url(url, keyword, emotions):
+    global current_image_name
+    global current_image_counter
     image_filename = url.split('.com/')[1].split('/')[1]
+    if current_image_name == image_filename:
+        current_image_counter = current_image_counter + 1
+    else:
+        current_image_counter = 1
+    current_image_name = image_filename
+    if len(emotions.split(',')) > 3:
+        separated_emotions = [', '.join(emotions.split(',')[0:2]), ', '.join(emotions.split(',')[2:])]
+        emotions = separated_emotions
+        print len(emotions)
+
     path_to_img = path_to_saved_images + keyword + '/'+image_filename
     img = cv2.imread(path_to_img,1)
-    cv2.putText(img, emotions, corner, font, fontScale, fontColor, lineType)
+    #print len(emotions)
+
+    if len(emotions) == 2:
+        text_to_show = str(current_image_counter)+ '. '+ str(emotions[0])
+        cv2.putText(img, text_to_show, corner, font, fontScale, fontColor, lineType)
+        cv2.putText(img, str(emotions[1]), (corner[0], corner[1]+50), font, fontScale, fontColor, lineType)
+    else:
+        text_to_show = str(current_image_counter)+ '. '+ emotions
+        cv2.putText(img, text_to_show, corner, font, fontScale, fontColor, lineType)
     cv2.imshow('image',img)
     cv2.waitKey(500)
+    return current_image_name
 
 def process_emotions(emotions):
     if '\n' in emotions:
-        emotions = ','.join(emotions.split('\n'))
+        emotions = ', '.join(emotions.split('\n'))
     return emotions
 
 results_and_indexes = open_csv_results(results_csv)
@@ -46,7 +70,9 @@ results = results_and_indexes[0]
 emotion_types_index = results_and_indexes[1]
 image_url_index = results_and_indexes[2]
 
+
 for i, r in enumerate(results):
+    #if not the first row (the header)
     if i > 0:
         keyword = ''
         for item in r:
@@ -56,4 +82,3 @@ for i, r in enumerate(results):
         if keyword == '':
             sys.exit('Exit: no keyword found.')
         process_url(r[image_url_index], keyword, emotions)
-        print '\n'
